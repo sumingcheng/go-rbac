864fc1657537c2341cd8d6784f69329ecf4446fa
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-11-19 19:54:51 +0800

    feat: 添加Dockerfile和Makefile
    添加了Dockerfile和Makefile文件，用于构建和部署应用程序。这些文件将帮助我们更方便地管理应用程序的依赖和部署过程。

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 )
 
 func initDB() *gorm.DB {
-	dsn := "root:admin123456@tcp(172.22.220.64:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:your_password@tcp(localhost:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("连接数据库失败: " + err.Error())
 	}
 	return db
 }
