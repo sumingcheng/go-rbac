62e56208ffaab3e3291b4be0eabf9b07f8daae4c
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-11-20 08:57:47 +0800

    fix: filename

--- service/role.go
+++ service/role.go
@@ -1,35 +1,35 @@
 package service
 
 import (
-    "github.com/jmoiron/sqlx"
+	"github.com/jmoiron/sqlx"
 )
 
 type RoleService struct {
-    db *sqlx.DB
+	db *sqlx.DB
 }
 
 func NewRoleService(db *sqlx.DB) *RoleService {
-    return &RoleService{db: db}
+	return &RoleService{db: db}
 }
 
 func (s *RoleService) CreateRole(name, description string) error {
-    _, err := s.db.Exec(
-        "INSERT INTO roles (name, description) VALUES (?, ?)",
-        name, description,
-    )
-    return err
+	_, err := s.db.Exec(
+		"INSERT INTO roles (name, description) VALUES (?, ?)",
+		name, description,
+	)
+	return err
 }
 
 func (s *RoleService) AssignPermission(roleID, permissionID int) error {
-    _, err := s.db.Exec(
-        "INSERT INTO role_permissions (role_id, permission_id) VALUES (?, ?)",
-        roleID, permissionID,
-    )
-    return err
+	_, err := s.db.Exec(
+		"INSERT INTO role_permissions (role_id, permission_id) VALUES (?, ?)",
+		roleID, permissionID,
+	)
+	return err
 }
 
 func (s *RoleService) GetRoleByID(id int) (*Role, error) {
-    var role Role
-    err := s.db.Get(&role, "SELECT * FROM roles WHERE id = ?", id)
-    return &role, err
+	var role Role
+	err := s.db.Get(&role, "SELECT * FROM roles WHERE id = ?", id)
+	return &role, err
 }
